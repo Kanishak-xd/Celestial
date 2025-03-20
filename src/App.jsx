e1a47d7ad5218da0810e21fe7071f7e591687baf
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-03-20 23:48:49 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        &copy; {new Date().getFullYear()} Your Website. All rights reserved.
+      </p>
+    </footer>
+  );
+}
